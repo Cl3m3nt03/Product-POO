d8a275c92a8d0e3967115c5184dbbdfaac95fcab
florianBounon <florian.bounon@edu.esiee-it.fr>
2025-01-29 10:51:07 +0100

    create pharmacy

---
+++ src/Controller/Pharmacy.java
@@ -0,0 +1,16 @@
+package Controller;
+
+public abstract class Pharmacy {
+    private String nom;
+    private String adresse;
+
+    public Pharmacy(String nom, String adresse) {
+        this.nom = nom;
+        this.adresse = adresse;
+    }
+
+    public void afficherDetails() {
+        System.out.println("Pharmacie: " + nom);
+        System.out.println("Adresse: " + adresse);
+    }
+}
